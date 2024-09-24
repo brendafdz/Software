f6a9df230c1077d1b76d69f9f2e5ac9c9383573c
brendafdz <brenda.fernandez@iteso.mx>
2024-09-23 23:54:20 -0600

    Version 1

---
+++ hoja_contable.py
@@ -0,0 +1,26 @@
+print ("Bienvenido")
+print("")
+nombre=input("Ingresa tu nombre: ")
+apellido=input("Ingresa tu apellido: ")
+print("")
+
+print("Ingresa los siguientes datos:")                             # g=gasto
+g1=input("No. Campo (máx. 5 dígitos): ")                           # des= descripcion del gasto
+des1= input("Descripción (máx. 20 letras): ")                      # cg=costo del gasto
+cg1=input(float("Monto mensual: "))
+print("")
+g2=input("No. Campo (máx. 5 dígitos): ")
+des2= input("Descripción (máx. 20 letras): ")
+cg2=input(float("Monto mensual: "))
+print("")
+g3=input("No. Campo (máx. 5 dígitos): ")
+des3= input("Descripción (máx. 20 letras): ")
+cg3=input(float("Monto mensual: "))
+print("")
+g4=input("No. Campo (máx. 5 dígitos): ")
+des4= input("Descripción (máx. 20 letras): ")
+cg4=input(float("Monto mensual: "))
+print("")
+g5=input("No. Campo (máx. 5 dígitos): ")
+des5= input("Descripción (máx. 20 letras): ")
+cg5=input(float("Monto mensual: "))
